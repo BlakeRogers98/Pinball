bab58f2d0f73d90bdc83b53b5d6e8fd70065a3f2
BlakeRogers98 <RogersBlake98@gmail.com>
2020-08-29 15:13:47 -0500

    Fix Stuff Hopefully

---
+++ Assets/Scripts/LauncherScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class LauncherScript : MonoBehaviour
+{
+
+    private Rigidbody launcher;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        launcher = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
