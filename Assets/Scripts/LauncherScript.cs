d8fe6969d0bfec49dca200cfbe4deb9f675c1109
BlakeRogers98 <RogersBlake98@gmail.com>
2020-08-29 18:16:02 -0500

    Launcher Scipt Update
    Launcher now can have a direction, have a compression speed, and have a different expansion speed

--- Assets/Scripts/LauncherScript.cs
+++ Assets/Scripts/LauncherScript.cs
@@ -8,39 +8,70 @@ public class LauncherScript : MonoBehaviour
 {
     public float compressSpeed;
     public float expandSpeed;
+    public int direction=0;
 
     private Rigidbody launcher;
     private Vector3 rotation;
-    
+    private Vector3 position;
+    private Vector3 moveDirection;
 
     // Start is called before the first frame update
     void Start()
     {
         launcher = GetComponent<Rigidbody>();
-        float x = launcher.transform.eulerAngles.x;
-        float y =launcher.transform.eulerAngles.y;
-        float z =launcher.transform.eulerAngles.z;
-        rotation = new Vector3(x, y, z);
+        rotation = launcher.transform.eulerAngles;
+        position = launcher.transform.position;
+        print(rotation);
+        print(position);
+        Direction();
+    }
+
+    void Direction()
+    {
+        switch (direction)
+        {
+            case 1:
+                moveDirection = new Vector3(1, 0, 0);
+                break;
+            case 2:
+                moveDirection = new Vector3(-1, 0, 0);
+                break;
+            case 3:
+                moveDirection = new Vector3(0, 1, 0);
+                break;
+            case 4:
+                moveDirection = new Vector3(0, -1, 0);
+                break;
+            case 5:
+                moveDirection = new Vector3(0, 0, 1);
+                break;
+            case 6:
+                moveDirection = new Vector3(0, 0, -1);
+                break;
+            default:
+                moveDirection = new Vector3(0, 0, 0);
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             //Vector3 movement = new Vector3(movementX, 0.0F, movementY);
 
-            launcher.AddForce(rotation * compressSpeed);
+            //transform.localPosition = new Vector3(pos_x+1*compressSpeed, pos_y, pos_z);
+            transform.position += moveDirection * Time.deltaTime * compressSpeed;
         }
         else {
             //Vector3 movement = new Vector3(movementX, 0.0F, movementY);
-
-            launcher.AddForce(rotation * expandSpeed);
+            transform.position -= moveDirection * Time.deltaTime * expandSpeed;
         }
     }
 }
