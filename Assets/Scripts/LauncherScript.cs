7ac960fdaf314b91555f523da80d3b2b3cf29c54
BlakeRogers98 <RogersBlake98@gmail.com>
2020-08-29 15:57:38 -0500

    Basic Script Launcher
    Creater Script for the Launcher to work

--- Assets/Scripts/LauncherScript.cs
+++ Assets/Scripts/LauncherScript.cs
@@ -6,18 +6,41 @@ using TMPro;
 
 public class LauncherScript : MonoBehaviour
 {
+    public float compressSpeed;
+    public float expandSpeed;
 
     private Rigidbody launcher;
+    private Vector3 rotation;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         launcher = GetComponent<Rigidbody>();
+        float x = launcher.transform.eulerAngles.x;
+        float y =launcher.transform.eulerAngles.y;
+        float z =launcher.transform.eulerAngles.z;
+        rotation = new Vector3(x, y, z);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Vector3 movement = new Vector3(movementX, 0.0F, movementY);
+
+            launcher.AddForce(rotation * compressSpeed);
+        }
+        else {
+            //Vector3 movement = new Vector3(movementX, 0.0F, movementY);
+
+            launcher.AddForce(rotation * expandSpeed);
+        }
     }
 }
