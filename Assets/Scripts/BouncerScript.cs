a7c5cf635a99ef67e38a69a58c4c4b8d102f8028
bryanhoneck <bryan.honeck@gmail.com>
2020-09-04 19:52:35 -0500

    Updated Camera and Bouncers!
    Camera is positioned well and the bouncers now operate!

---
+++ Assets/Scripts/BouncerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncerScript : MonoBehaviour
+{
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float explosionStrength = 1500f;
+
+    void OnCollisionEnter(Collision _other)
+    {
+        _other.rigidbody.AddExplosionForce(explosionStrength, this.transform.position, 5);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
